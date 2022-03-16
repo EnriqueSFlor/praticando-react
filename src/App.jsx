1cfee3a46e5c03b71389c97266e4cdb33a1f3a8d
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-03-15 22:31:51 -0300

    praticando react

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import { Children } from "react";
+import React, { Component } from "react";
 
-const Equipe = (props) => {
-  return (
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-      <Social facebook={props.facebook} />
-      <hr />
-    </div>
-  );
-};
+class Equipe extends Component {
+  render() {
+    return (
+      <div>
+        <Sobre
+          nome={this.props.nome}
+          cargo={this.props.cargo}
+          idade={this.props.idade}
+        />
+        <Social facebook="http://google.com.br" />
+        <hr />
+      </div>
+    );
+  }
+}
 
-const Sobre = (props) => {
-  return (
-    <div>
-      <h2>{props.nome}</h2>
-      <h3>{props.cargo}</h3>
-      <h3>{props.idade}</h3>
-    </div>
-  );
-};
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Olá sou o(a) {this.props.nome}</h2>
+        <h3>{this.props.cargo}</h3>
+        <h3>{this.props.idade}</h3>
+      </div>
+    );
+  }
+}
 
 const Social = (props) => {
   return (
     <div>
       <a href={props.facebook}>Facebook </a>
-      <a>Linkdin </a>
-      <a>Youtube </a>
+      <a href={props.facebook}>Instagran </a>
     </div>
   );
 };
@@ -34,24 +41,8 @@ export default function App() {
   return (
     <div>
       <h1>Conheça nossa equipe</h1>
-      <Equipe
-        nome="Enrique"
-        cargo="Programador"
-        idade="28"
-        facebook="https://google.com/"
-      />
-      <Equipe
-        nome="Regina"
-        cargo="Aposentada"
-        idade="63"
-        facebook="https://google.com/"
-      />
-      <Equipe
-        nome="Eduardo"
-        cargo="Motoboy"
-        idade="34"
-        facebook="https://google.com/"
-      />
+      <Equipe nome="Enrique" cargo="Programador" idade="28" />
+      <Equipe nome="Eduardo" cargo="Motoboy" idade="33" />
     </div>
   );
 }
