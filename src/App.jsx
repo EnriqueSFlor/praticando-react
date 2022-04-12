531913d017aa997dde68a2a90f3cb3b89d4f98a2
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-04-11 21:26:54 -0300

    novo arquivo

--- src/App.jsx
+++ src/App.jsx
@@ -4,46 +4,17 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nome: "Matheus",
-      contador: 0,
+      hora: "00:00:00",
     };
-
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
-    this.mudarNome = this.mudarNome.bind(this);
   }
 
-  aumentar() {
-    let state = this.state;
-    state.contador += 1;
-
-    this.setState(state);
-  }
-
-  diminuir() {
-    let state = this.state;
-    state.contador -= 1;
-    this.setState(state);
-  }
-
-  mudarNome() {
-    let state = this.state;
-    state.nome = "Enrique";
-    this.setState(state);
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() });
+    }, 1000);
   }
 
   render() {
-    return (
-      <div>
-        <h1>Contador</h1>
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button> <hr />
-          <p>{this.state.nome}</p>
-          <button onClick={this.mudarNome}>Mudar nome</button>
-        </h3>
-      </div>
-    );
+    return <h1>Meu Projeto {this.state.hora}</h1>;
   }
 }
